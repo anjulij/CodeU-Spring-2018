5279913607b019f8a22bf18387ae1943a384ff53
Anjuli J <anjulij@Khoranhlais-MBP.xavier.xula.local>
2018-03-28 10:03:56 -0500

    added RegisterServlet and things associated with it

---
+++ src/main/java/codeu/controller/RegisterServletTest.java
@@ -0,0 +1,37 @@
+/*Anjuli*/
+package codeu.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+public class RegisterServletTest {
+    private RegisterServlet registerServlet;
+    private HttpServletRequest mockRequest;
+    private PrintWriter mockPrintWriter;
+    private HttpServletResponse mockResponse;
+
+    @Before
+    public void setup() throws IOException{
+        registerServlet = new RegisterServlet();
+        mockRequest = Mockito.mock(HttpServletRequest.class);
+        mockPrintWriter = Mockito.mock(PrintWriter.class);
+        mockResponse = Mockito.mock(HttpServletResponse.class);
+
+        Mockito.when(mockResponse.getWriter()).thenReturn(mockPrintWriter);
+    }
+
+    @Test
+    public void testDoGet()throws IOException, ServletException{
+        registerServlet.doGet(mockRequest,mockResponse);
+
+        Mockito.verify(mockPrintWriter).println("<h1>RegisterServlet GET request.</h1>");
+    }
+
+}
