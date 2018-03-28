0cdbd08a918fca0e02512190eb96d4947a7af039
Anjuli J <anjulij@Khoranhlais-MBP.xavier.xula.local>
2018-03-28 11:44:18 -0500

    added UserStore to RegisterServlet

--- src/main/java/codeu/controller/RegisterServlet.java
+++ src/main/java/codeu/controller/RegisterServlet.java
@@ -6,12 +6,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import codeu.model.store.basic.UserStore;
 
 /**
  * Servlet class responsible for user registration
  */
 
 public class RegisterServlet extends HttpServlet {
+
+    /**
+     * Store class that gives access to Users.
+     */
+
+    private UserStore userStore;
+
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response)
         throws IOException, ServletException{
@@ -34,6 +42,26 @@ public class RegisterServlet extends HttpServlet {
         response.getWriter().println("<p>Username: " + username + "</p>");
         response.getWriter().println("<p>Password: " + password + "</p>");
 
+    }
+
+    /**
+     * Set up state for handling registration-related requests.
+     * This method is only called when running a server, not when running in a test
+     */
+
+    @Override
+    public void init() throws ServletException{
+        super.init();
+        setUserStore(UserStore.getInstance());
+    }
+
+    /**
+     * Sets the UserStore used by this servlet. This function provides a common setup method
+     * for use by the test framework or the servlet's init() function.
+     */
+
+    void setUserStore(UserStore userStore){
+        this.userStore = userStore;
     }
 
 
