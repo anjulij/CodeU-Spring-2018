85a3afc8a8fa651ce883aec164a15c42fb656365
Neil Jones <ncjones@codeustudents.com>
2018-04-29 21:19:17 -0700

    Adjust unit test to pass in encrypted password

--- src/test/java/codeu/model/data/UserTest.java
+++ src/test/java/codeu/model/data/UserTest.java
@@ -29,11 +29,11 @@ public class UserTest {
     Instant creation = Instant.now();
     String password = "password";
 
-    User user = new User(id, name, hashedPassword, creation);
+    User user = new User(id, name, password, creation);
 
     Assert.assertEquals(id, user.getId());
     Assert.assertEquals(name, user.getName());
-    Assert.assertEquals(hashedPassword, user.getPassword());
+    Assert.assertEquals(password, user.getPassword());
     Assert.assertEquals(creation, user.getCreationTime());
   }
 }
