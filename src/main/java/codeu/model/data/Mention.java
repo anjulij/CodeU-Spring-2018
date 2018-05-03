8cd59171ac8edcf0423d96c665fe5f411a0f9d88
EstherBrown1 <30759092+EstherBrown1@users.noreply.github.com>
2018-05-03 16:27:29 -0400

    Update Mention.java

--- src/main/java/codeu/model/data/Mention.java
+++ src/main/java/codeu/model/data/Mention.java
@@ -1,23 +1,19 @@
 // Copyright 2017 Google Inc.
 
 //package codeu.model.data;
-
 import java.time.Instant;
 import java.util.UUID;
-
 /**
 The Mention class represent just a single mention and collects the data for a single mention.
 
  */
 public class Mention {
-
 	  private final UUID userWhoWasMentioned;// the id of the user who was mentioned
 	  private final UUID userWhoDidTheMentioning; // the id of the user who did the mentioning
 	  private final int start;// represents the lower bound of the number of String Indices
 	  private final int end;// represents the upper bound of the number of String Indices
 	  private final Instant creationTime;// represents the creation time of this Mention
-	  
-	  
+	
 	  public Mention(UUID userWhoWasMentioned, UUID userWhoDidTheMentioning, int start, int end, Instant creationTime) {
 	    this.userWhoWasMentioned = userWhoWasMentioned;
 	    this.userWhoDidTheMentioning = userWhoDidTheMentioning;
